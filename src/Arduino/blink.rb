bc4e71ea3d404d3059676b615c5847c2f78abb71
Gustavo Pereira da Fonseca <39752736+Gustavo053@users.noreply.github.com>
2020-10-20 22:04:19 -0300

    flashing the arduino internal led

---
+++ src/Arduino/blink.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+$:.unshift File.expand_path '../lib', File.dirname(__FILE__)
+require 'rubygems'
+require 'arduino_firmata'
+
+arduino = ArduinoFirmata.connect "COM3"
+puts "firmata version #{arduino.version}"
+
+arduino.pin_mode 13, ArduinoFirmata::OUTPUT
+
+stat = true
+loop do
+  puts stat
+  arduino.digital_write 13, stat
+  stat = !stat
+  sleep 0.1
+end
